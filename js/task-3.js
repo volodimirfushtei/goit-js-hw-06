20751b227822ff383492bbe732ea7faf2103eb13
Volodimir <161057867+volodimirfushtei@users.noreply.github.com>
2024-06-19 19:13:27 +0300

    add task 3

---
+++ js/task-3.js
@@ -0,0 +1,27 @@
+"use strict";
+class StringBuilder {
+  #value;
+  constructor(initialValue = "") {
+    this.#value = initialValue;
+  }
+  getValue() {
+    return this.#value;
+  }
+  padEnd(str) {
+    this.#value += str;
+  }
+  padStart(str) {
+    this.#value = str + this.#value;
+  }
+  padBoth(str) {
+    this.#value = String.prototype.concat.call(str, this.#value, str);
+  }
+}
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
